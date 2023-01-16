f31d2e460cedfabbe19f0f762cc99650dbbd8cae
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-16 11:46:12 -0700

    Rename DEFAULT_CONFIG to DEFAULT_THEME_CONFIG for validateThemeConfig.

--- src/validateThemeConfig.ts
+++ src/validateThemeConfig.ts
@@ -4,7 +4,7 @@ import type {
 } from '@docusaurus/types';
 import { Joi } from '@docusaurus/utils-validation';
 
-export const DEFAULT_CONFIG = {
+export const DEFAULT_THEME_CONFIG = {
     githubAuthorizationRedirectUrl: 'https://kgevadn5a2.execute-api.us-east-1.amazonaws.com/production/DocusaurusEditor_handleOAuthRedirect',
 };
 
@@ -13,10 +13,10 @@ export const DEFAULT_CONFIG = {
 export const ThemeConfigSchema = Joi.object<ThemeConfig>({
     editor: Joi.object({
         githubAuthorizationRedirectUrl:
-            Joi.string().default(DEFAULT_CONFIG.githubAuthorizationRedirectUrl),
+            Joi.string().default(DEFAULT_THEME_CONFIG.githubAuthorizationRedirectUrl),
     })
         .label('themeConfig.editor')
-        .default(DEFAULT_CONFIG),
+        .default(DEFAULT_THEME_CONFIG),
 });
 
 export function validateThemeConfig({
