cadb382e1ae3f56eb4cf2b4eef95dfdca0fcd040
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-16 11:34:06 -0700

    Move @mui/material/styles to types ambient module.

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+declare module '@mui/material/styles' {
+    interface BreakpointOverrides {
+        xs: false;
+        sm: false;
+        md: false;
+        lg: false;
+        xl: false;
+        mobile: true;
+    }
+}
