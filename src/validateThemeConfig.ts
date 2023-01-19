101cb90cad763f7cf77757d751616753f73affdc
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-19 05:18:58 -0700

    Fix DEFAULT_THEME_CONFIG missing type declaration for validateThemeConfig.

--- src/validateThemeConfig.ts
+++ src/validateThemeConfig.ts
@@ -1,10 +1,11 @@
+import type { EditorThemeConfig } from '@docusaurus/theme-editor';
 import type {
     ThemeConfig,
     ThemeConfigValidationContext
 } from '@docusaurus/types';
 import { Joi } from '@docusaurus/utils-validation';
 
-const DEFAULT_THEME_CONFIG = {
+const DEFAULT_THEME_CONFIG: EditorThemeConfig = {
     // TODO(dnguyen0304): Extract as a configuration setting.
     githubAuthorizationRedirectUrl: 'https://kgevadn5a2.execute-api.us-east-1.amazonaws.com/production/DocusaurusEditor_handleOAuthRedirect',
 };
